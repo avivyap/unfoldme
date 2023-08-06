b446f862be95dd1e3b5f01ceff8f663fe59a0138
aviv <141031195+avivyap@users.noreply.github.com>
2023-08-07 00:20:59 +0200

    Update unfoldme.sh

--- unfoldme.sh
+++ unfoldme.sh
@@ -6,22 +6,34 @@ sleep 3
 
 echo -e "\e[33m[+] Primero se va a hacer una actualización del sistema\e[0m"
 
-sleep 2
+sleep 3
 
 apt update
 sudo apt upgrade -y
 
+sleep 3
+
 echo -e "\e[33m[+] Ahora se va a instalar php y apache2\e[0m"
+
+sleep 3
 
 sudo apt install php -y
 sudo apt install apache2 -y
 
+sleep 3
+
 echo -e "\e[33m[+] Ahora se instalará docker y docker-compose\e[0m"
+
+sleep 3
 
 sudo apt install docker -y
 sudo apt install docker-compose -y
 
+sleep 3
+
 echo -e "\e[33m[+] Ahora se instalaran las herramientas de hacking\e[0m" 
+
+sleep 3
 
 sudo apt install nmap -y
 sudo apt install arp-scan -y
@@ -44,12 +56,18 @@ sudo apt install steghide -y
 sudo apt install wfuzz -y
 sudo apt install whatweb -y
 
+sleep 3
+
 echo -e "\e[33m[+] Por ultimo se descargaran los repositorios\e[0m"
+
+sleep 3
 
 git clone https://github.com/danielmiessler/SecLists
 git clone https://github.com/AonCyberLabs/PadBuster
 git clone https://gitlab.com/exploit-database/exploitdb
 git clone https://github.com/sqlmapproject/sqlmap
+
+sleep 3
 
 echo -e "\e[33m[+] Se han instalado las siguientes herramientas\e[0m"
 
